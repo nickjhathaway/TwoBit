c5bb3415013fdac3c71124d3ccef9facd237c75c
Arjan van der Velde <vandervelde.ag@gmail.com>
2014-11-30 14:52:20 -0500

    .

--- src/main.cpp
+++ src/main.cpp
@@ -12,19 +12,21 @@ int main(int argc, char** argv)
 	std::cout << tbs << std::endl;
 
 	std::vector<char> bla;
+
 	tbs.getSequence(bla, 9990, 11000);
 	std::cout << "bla.size(): " << bla.size() << std::endl;
 	std::cout << "bla.capacity(): " << bla.capacity() << std::endl;
 	std::cout << std::string(bla.begin(), bla.end()) << std::endl;
+
 	tbs.getSequence(bla, 9990, 11000, true);
 	std::cout << "bla.size(): " << bla.size() << std::endl;
 	std::cout << "bla.capacity(): " << bla.capacity() << std::endl;
 	std::cout << std::string(bla.begin(), bla.end()) << std::endl;
+
 	tbs.getSequence(bla, 0, 2000000000);
-	std::cout << "request: 2000000000" << std::endl;
 	std::cout << "bla.size(): " << bla.size() << std::endl;
 	std::cout << "bla.capacity(): " << bla.capacity() << std::endl;
-//	tb.test();
+
 	return 0;
 }
 
