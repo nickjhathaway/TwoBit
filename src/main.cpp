0415fc7a4f5cb86df4668a8134f8174c178b7ee0
Arjan van der Velde <vandervelde.ag@gmail.com>
2015-01-10 15:39:23 -0500

    output exception to stderr

--- src/main.cpp
+++ src/main.cpp
@@ -41,7 +41,7 @@ int main(int argc, char** argv)
 
 	} catch (std::exception& e)
 	{
-		std::cout << "Error: " << e.what() << std::endl;
+		std::cerr << "Error: " << e.what() << std::endl;
 		return 1;
 	}
 
