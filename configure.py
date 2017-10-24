fd6b98cecd5811892a56ae22fe1924efcaab0080
nickjhathaway <nickjhathaway@gmail.com>
2017-10-23 22:07:15 -0400

    update for dev version;

--- configure.py
+++ configure.py
@@ -7,7 +7,7 @@ from utils import Utils
 from genFuncs import genHelper
 def main():
     name = "TwoBit"
-    libs = "bibcpp:v2.5.1"
+    libs = "bibcpp:develop"
     args = genHelper.parseNjhConfigureArgs()
     cmd = genHelper.mkConfigCmd(name, libs, sys.argv)
     Utils.run(cmd)
